90a949d6de6731b75e466d206f4a1b3b479a6798
Carl-Henrik Skårstedt <cskarstedt@gmail.com>
2020-11-09 23:50:14 +0100

    tweaked the code view spacing more

--- src/CodeView.cpp
+++ src/CodeView.cpp
@@ -282,7 +282,7 @@ void CodeView::Draw(int index)
 				if (!srcLine) { srcLine = GetListing(read, nullptr, nullptr); }
 				if (srcLine) {
 					ImGui::SameLine();
-					strl_t col = (showAddress ? 6 : 0) + (showBytes ? 7 : 0) + (showRefs ? 9 : 0) + (showLabels ? 6 : 0) + 5 + (spaces + 3) / 4;
+					strl_t col = (showAddress ? 6 : 0) + (showBytes ? 9 : 0) + (showRefs ? 9 : 0) + (showLabels ? 6 : 0) + 9 + (spaces + 3) / 4;
 					ImVec2 srcPos(linePos.x + col * fontCharWidth, linePos.y);
 					ImGui::SetCursorPos(srcPos);
 					ImGui::PushStyleColor(ImGuiCol_Text, C64_YELLOW);
